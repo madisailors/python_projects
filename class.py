aee565040213cb8be8e8fc72ce5459dff72299f5
madisailors <109822907+madisailors@users.noreply.github.com>
2022-10-04 14:27:08 -0700

    submission

---
+++ class.py
@@ -0,0 +1,17 @@
+def __init__(self, name, address):
+    self.name = name
+    self.address = address
+    
+class Person:
+    name = 'No name provided'
+    address = ' '
+ 
+class Student(Person):
+    GPA = 3.8
+   
+
+class Teacher(Person):
+    Department = 'Physical Education'
+    Salary = 35,000
+    
+    

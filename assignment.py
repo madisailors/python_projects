1334c643a457e46991da21e775374f0ce8bf4295
madisailors <109822907+madisailors@users.noreply.github.com>
2022-10-04 14:32:58 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -5,28 +5,25 @@ conn = sqlite3.connect('assignment.db')
 with conn:
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS tbl_documents( \
-        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-        col_name TEXT \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \ 
+        col_name TEXT \ #creating a database and adding a column 
         )")
     conn.commit()
-conn.close()
+conn.close() #closing connection
                 
-conn = sqlite3.connect('assignment.db')
+conn = sqlite3.connect('assignment.db') #connecting to database
 
 fileList = ('information.docx', 'hello.txt', 'myImage.png', \
             'myMovie.mpg', 'world.txt', 'data.pdf','myphoto.jpg')
 
 for i in fileList:
-    if i.endswith('.txt'):
+    if i.endswith('.txt'): #iterating through fileList to find docs ending in '.txt'
         with conn:
             cur = conn.cursor()
             cur.execute("INSERT INTO tbl_documents(col_name) \
-                VALUES (?)", \
+                VALUES (?)", \ #adding selected docs to table
                 (i,))
-            cur.execute("INSERT INTO tbl_documents(col_name) \
-                VALUES(?)", \
-                (i,))
-            conn.commit()
+            conn.commit() #comitting changes to table
 conn.close()
 
 

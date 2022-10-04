0ad0df33e185c5957dc1ef1337a831dd9ab0fd4e
madisailors <109822907+madisailors@users.noreply.github.com>
2022-10-04 14:29:55 -0700

    Update class.py

--- class.py
+++ class.py
@@ -2,11 +2,11 @@ def __init__(self, name, address):
     self.name = name
     self.address = address
     
-class Person:
+class Person: #All child classes will have name and address attributes
     name = 'No name provided'
     address = ' '
  
-class Student(Person):
+class Student(Person): #Child class of Person, adding unique attributes
     GPA = 3.8
     Grade = 8
 

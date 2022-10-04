db04816ae2f1968fd9e1e281395de352f3c65b28
madisailors <109822907+madisailors@users.noreply.github.com>
2022-10-04 13:25:48 -0700

    draft

--- assignment.py
+++ assignment.py
@@ -13,6 +13,12 @@ conn.close()
                 
 conn = sqlite3.connect('assignment.db')
 
+fileList = ('information.docx', 'hello.txt', 'myImage.png', \
+            'myMovie.mpg', 'world.txt', 'data.pdf','myphoto.jpg')
+
+for i in range(1,4):
+    print(fileList[1],fileList[4])
+    
 with conn:
     cur = conn.cursor()
     cur.execute("INSERT INTO tbl_documents(col_name) \
@@ -26,11 +32,7 @@ conn.close()
 
 
  
-fileList = ('information.docx', 'hello.txt', 'myImage.png', \
-            'myMovie.mpg', 'world.txt', 'data.pdf','myphoto.jpg')
 
-for i in range(1,4):
-    print(fileList[1],fileList[4])
 
                
 

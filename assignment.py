49de0f35ec3d925d1c4f5e6b06de12ff9849b948
madisailors <109822907+madisailors@users.noreply.github.com>
2022-10-04 10:03:36 -0700

    draft

--- assignment.py
+++ assignment.py
@@ -11,3 +11,22 @@ with conn:
     conn.commit()
 conn.close()
                 
+conn = sqlite3.connect('assignment.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("INSERT INTO tbl_documents(col_name) \
+        VALUES (?)", \
+        ('hello.txt',))
+    cur.execute("INSERT INTO tbl_documents(col_name) \
+        VALUES(?)", \
+        ('world.txt',))
+    conn.commit()
+conn.close()
+
+
+ 
+fileList = ('information.docx', 'hello.txt', 'myImage.png', \
+            'myMovie.mpg', 'world.txt', 'data.pdf','myphoto.jpg')
+
+               

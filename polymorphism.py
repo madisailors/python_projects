afc23e656d459509564322ce48ce99ab71512d5e
madisailors <109822907+madisailors@users.noreply.github.com>
2022-10-06 15:41:14 -0700

    Update polymorphism.py

--- polymorphism.py
+++ polymorphism.py
@@ -9,6 +9,7 @@ class Electronic:
     def information(self): 
         msg = "\nDevice Type: {}\nBrand: {}\nSize: {}\nMemory: {}".format(self.device,self.brand,self.size,self.memory)
         return msg
+    
 
 #child class
 class Laptop(Electronic):
@@ -16,11 +17,19 @@ class Laptop(Electronic):
     brand = "Apple"
     size = "15.6 inch"
 
+    def information(self):
+        msg = "\nCongrats, you won a {} {} {} with {} of memory!".format(self.size,self.brand,self.device,self.memory)
+        return msg
+
 #child class
 class Tablet(Electronic):
     device = "HP Chromebook X2 11"
     brand = "Google"
-    size = "11 inch"
+    size = "12 inch"
+
+    def information(self):
+        msg = "\nCongrats, you won a {} {} {} with {} of memory!".format(self.size,self.brand,self.device,self.memory)
+        return msg
 
 
 if __name__ == "__main__":

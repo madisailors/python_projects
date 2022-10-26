02c3bcaac16fa2c228e18679a819096568996278
madisailors <109822907+madisailors@users.noreply.github.com>
2022-10-26 15:14:26 -0700

    draft

--- web_page_generator.py
+++ web_page_generator.py
@@ -9,11 +9,16 @@ class ParentWindow(Frame):
 
         #default html button
         self.btn = Button(self.master, text="Default HTML Page", width=30, height=2, command=self.defaultHTML)
-        self.btn.grid(padx=(10,10), pady=(10,10))
+        self.btn.grid(row=1, column=1, padx=(10,10), pady=(10,10))
 
         #input button
         self.btn = Button(self.master, text="Submit Custom Text", width=30, height=2, command=self.customHTML)
-        self.btn.grid(padx=(10,10), pady=(10,10))
+        self.btn.grid(row=1,column=0, padx=(10,10), pady=(10,10))
+
+        self.L1 = Label(text="Enter custom text or click default HTML page button")
+        self.L1.grid(row=0, column=0, padx=(10,10), pady=(10,10))
+        self.entry = Entry(width=75)
+        self.entry.grid(row=0, column=1, padx=(10,10), pady=(10,10))
 
     def defaultHTML(self):
         htmlText = "Stay tuned for our amazing summer sale!"
@@ -26,14 +31,17 @@ class ParentWindow(Frame):
     
     #defining function for text entry
     def customHTML(self):
-        L1 = Label(top, text="Enter custom text or click default HTML page button")
-        L1.grid(row=0, column=0, zpadx=(10,1), pady=(10,1))
-        entry = Entry(top, width=75)
-        entry.grid(row=1, column=1, padx=(10,10), pady=(10,10))
+       v = self.entry
+       customText = v
+       customFile = open("index.html", "w")
+       customContent = "<html>\n<body>\n<h1>" + customText + "</h1>\n</body>\n</html>"
+       customFile.write(customContent)
+       customFile.close()
+       webbrowser.open_new_tab("index.html")
 
     #defining function to return data entry
     def get_data(self):
-        L1.config(text = entry.get())
+        self.L1.config(text = self.entry.get())
 
 
 if __name__ == "__main__":
